f33877c39134a68b475bfad44ed0d906cafb9e36
g g <gabedotgutierrez@gmail.com>
2024-09-26 11:36:25 -0400

    index ejs made

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const express = require("express");
 
 const app = express();
 
+// GET /
+app.get("/", async (req, res) => {
+    res.render("index.ejs");
+  });
+  
 app.listen(3000, () => {
   console.log("In the year 3000");
 });
