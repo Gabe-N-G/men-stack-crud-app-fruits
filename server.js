1b038d95516e22ee276a481f3c58e20c27044d6d
g g <gabedotgutierrez@gmail.com>
2024-09-26 12:09:48 -0400

    added mongo connection to fruits database

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 // Here is where we import modules
 // We begin by loading Express
 const express = require("express");
-
+const dotenv = require("dotenv"); // require package
+dotenv.config(); // Loads the environment variables from .env file
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGODB_URI);
+// log connection status to terminal on start
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
 
 // GET /
 app.get("/", async (req, res) => {
     res.render("index.ejs");
-  });
+});
   
 app.listen(3000, () => {
   console.log("In the year 3000");
