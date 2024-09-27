9601dea344799b00dce2794efe7cd17f91f359c0
g g <gabedotgutierrez@gmail.com>
2024-09-27 10:38:23 -0400

    MVC done! Take you for a ride

--- server.js
+++ server.js
@@ -63,6 +63,32 @@ app.delete("/fruits/:fruitId", async (req, res) => {
   res.redirect("/fruits");
 });
 
+// Edit route!
+app.get("/fruits/:fruitId/edit", async (req, res) => {
+  const foundFruit = await Fruit.findById(req.params.fruitId);
+  console.log(foundFruit);
+  res.render("fruits/edit.ejs", {
+    fruit: foundFruit,
+  });
+});
+
+//PUT (edit)
+
+app.put("/fruits/:fruitId", async (req, res) => {
+  // Handle the 'isReadyToEat' checkbox data
+  if (req.body.isReadyToEat === "on") {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+  
+  // Update the fruit in the database
+  await Fruit.findByIdAndUpdate(req.params.fruitId, req.body);
+
+  // Redirect to the fruit's show page to see the updates
+  res.redirect(`/fruits/${req.params.fruitId}`);
+});
+
 
 //console log for saying we are connected.
 app.listen(3000, () => {
